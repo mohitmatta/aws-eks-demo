33f49b3f964fd036314854b54d0e9ed35bdb755f
Mohit Matta <54612175+mohitmatta@users.noreply.github.com>
2026-01-02 19:08:30 -0600

    Implement FastAPI stock retrieval endpoint

---
+++ stock-app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from typing import Optional
+import json
+
+app = FastAPI()
+
+@app.get('/') # get method to empty endpoint
+def first_response():
+    return {"response": "first"}
+
+class Stock(BaseModel):
+    symbol: str
+    stockname: str
+    lastsale: str
+    country: str
+    ipoyear: Optional[int] = None
+    
+with open('./stock.json', 'r') as f:
+    stocks = json.load(f)['stocks']
+
+@app.get('/stock/{stock_symbol}', status_code=200)
+def get_stock(stock_symbol: str) -> Stock:
+    stock = [stock for stock in stocks if stock['symbol'] == stock_symbol]
+    if len(stock) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"No stock {stock_symbol} found."
+         )
+    return stock[0]
